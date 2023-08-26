4616cee90c6bd15e87d6a864cfa8ab1e68731659
Caelan Miller <98186985+caelanjmiller@users.noreply.github.com>
2023-08-26 17:37:06 -0500

    Restructuring & Table Generation

--- app.py
+++ app.py
@@ -1,25 +1,59 @@
-from dash import dash, Dash, Input, Output, callback, State, dcc
-from dash.exceptions import PreventUpdate
 from layout.components import *
+from dash import Dash, html, Input, Output
+from dash.exceptions import PreventUpdate
+from calendar_generation import retrieve_current_date
+from scraping_utils import processed_excel_file
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 app = Dash(
     __name__,
-    use_pages=True,
     suppress_callback_exceptions=True,
     external_stylesheets=[dbc.themes.MINTY, dbc.icons.FONT_AWESOME, dbc_css],
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"},
     ],
 )
-server = app.server
-app.title = "WUSTL DBBS PhD Stipend Pay Dates"
+# server = app.server
 app._favicon = "assets/favicon.ico"
-
+app.title = "WUSTL DBBS Stipend Pay Dates"
 app.layout = dbc.Container(
-    fluid=True,
-    children=[dcc.Store(id="stored-data", storage_type="memory"), dash.page_container],
+    fluid=False,
+    children=[
+        site_nav,
+        dbc.Row(
+            className="justify-content-center align-items-center",
+            id="body-content",
+            children=[
+                date_query_button,
+            ],
+        ),
+        page_footer,
+    ],
 )
+
+
+@app.callback(
+    Output(component_id="body-content", component_property="children"),
+    Input(component_id="date-btn", component_property="n_clicks"),
+    prevent_initial_call=True,
+)
+def update_date_header(n_clicks):
+    current_date = retrieve_current_date()
+    paydate_dataframe = processed_excel_file
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        result_component = html.Div(
+            children=[
+                html.H1(
+                    f"Today's Date: {current_date}", style={"text-align": "center"}
+                ),
+                dbc.Table.from_dataframe(
+                    paydate_dataframe, striped=True, bordered=True, hover=True
+                ),
+            ]
+        )
+        return result_component
 
 
 # Development Testing
--- pages/result_page.py
+++
@@ -1,4 +0,0 @@
-from layout.components import dbc
-from dash import dcc, register_page, State, callback, Input, Output
-from dash.exceptions import PreventUpdate
-from scraping_utils import processed_excel_file
